665b183e7b46af2d4a12453e72d57019b8081c51
benedetta-alten <benedetta.ippoliti@alten.it>
2025-05-23 10:39:11 +0200

    header changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [HeaderComponent], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'blu-meteo';
 }
+
+
+
